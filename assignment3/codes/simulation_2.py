b7804e2e13916532a46a5579dc9b28b8e297476b
Aditya11013 <131709284+Aditya11013@users.noreply.github.com>
2023-05-18 01:15:37 +0530

    Add files via upload

--- assignment3/codes/simulation_2.py
+++ assignment3/codes/simulation_2.py
@@ -1,24 +1,39 @@
 import random
-def sim(k, N):
-  trial_count = 0
-  for i in range(N):
-    # simulate a 5 question test
-    test_count = 0
-    for j in range(5):
-      # we'll say you got it right if you get a 1, else it's wrong
-      c = random.randint(1,3)
-      if c == 1:
-        test_count += 1
-    # after the test see how many they got right, if it's k
-    if test_count == k:
-      trial_count += 1
-  # after all the trials return the probability
-  return trial_count/N
-# getting 4 and 5 answers correct on a 5 Questions test
-print(sim(4, 1_000_000),sim(5,1_000_000))
-#probability of getting  k answers correct by guessing
-four=sim(4,1_000_000)
-five=sim(5,1_000_000)
-#probability of getting four or more answers correct by guessing
-greaterthanfour=four+five
-print(greaterthanfour)
+import scipy.stats as stats
+
+# Set the number of questions and possible answers
+num_questions = 5
+num_answers = 3
+
+# Set the number of simulations
+num_simulations = 100000
+
+# Set the probability of guessing the correct answer
+guess_prob = 1 / num_answers
+
+# Initialize the count of successful simulations
+success_count = 0
+
+# Perform the simulations
+for _ in range(num_simulations):
+    correct_count = 0  # Count of correct answers
+
+    # Guess the answers for each question
+    for _ in range(num_questions):
+        if random.random() < guess_prob:
+            correct_count += 1
+
+    # Check if the candidate got four or more correct answers
+    if correct_count >= 4:
+        success_count += 1
+
+# Calculate the simulated probability
+simulated_probability = success_count / num_simulations
+
+# Calculate the theoretical probability using the binomial distribution
+theoretical_probability = 1 - stats.binom.cdf(3, num_questions, guess_prob)
+
+# Print the results
+print("Simulated Probability:", simulated_probability)
+print("Theoretical Probability:", theoretical_probability)
+
